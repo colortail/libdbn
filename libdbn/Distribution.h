ee7eed149ba71b9362b4d22b825c890ae3cd0cc3
Tan G Van <tangoghvan@126.com>
2015-11-06 23:10:00 +0800

    rm Distribution.h .etc

--- libdbn/Distribution.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-//CPD条件概率分布，POT势函数
-enum DistType { CPD, POT };
-
-class Distribution{
-public:
-	virtual DistType distType() = 0;
-};
--- libdbn/StdInOutUtils.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "TableFactor.h"
-
-class StdInOutUtils {
-public:
-	static void showTableFactor(TableFactor&);
-	//static void scanTableFactor(TableFactor&);
-};
